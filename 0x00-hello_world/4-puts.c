740e334f08365c421254ea5d06c119d3d050cb12
Chukwukere <nwogbechukwukere@gmail.com>
2022-09-09 08:14:42 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,8 @@
-putsme
+#include <stdio.h>
+/**
+* main - Printing
+*
+*Return: Always 0 (Successful)
+*/
+int main(void)
+{puts ("\"Programming is like building a multilingual puzzle*);return (0);}
