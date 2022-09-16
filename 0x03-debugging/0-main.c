5838f84ad89582dee37f66d4ff7c27bbbc14af96
Chukwukere <nwogbechukwukere@gmail.com>
2022-09-15 21:52:58 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - tests function prints if integer is positive or negative
+* main - tests funtion that priests if integer is positive or negative
 * Return: 0
 */
 
@@ -13,4 +13,5 @@ i - 0;
 positive_or_negative(i);
 
 return (0);
+
 )
