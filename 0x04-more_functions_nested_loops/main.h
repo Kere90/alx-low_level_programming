aa4391fb4b70958ac57ed19f7c69c7786be0418c
Chukwukere <nwogbechukwukere@gmail.com>
2022-09-16 16:23:18 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,4 +16,9 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_numbers(int n);
-int _putchar
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif /* MAIN_H */
