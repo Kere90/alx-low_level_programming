0bd0fd42a04beb259e1b67053f681995b7404fff
Chukwukere <nwogbechukwukere@gmail.com>
2022-09-22 17:24:08 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
- /**
-   * _strcmp - Compares two strings
-   * @s1: The first string
-   * @s2: The second string
-   *
-   * Return: int value
-   */
-  int _strcmp(char *s1, char *s2)
-  {
+/**
+ * _strcmp - Compares two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: int value
+ */
+int _strcmp(char *s1, char *s2)
+{
 	 int a = 0, b = 0, c = 0, r = 0, lim;
 
 	 while (s1[a])
@@ -45,4 +45,5 @@
 
 		 C++;
 	}
-  }
+  
+}
