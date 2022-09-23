6e9151034d71ec29568348b32a6b4d94e4a284e4
Chukwukere <nwogbechukwukere@gmail.com>
2022-09-22 17:09:24 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,8 +7,8 @@
    *
    * Return: int value
    */
- int _strcmp(char *s1, char *s2)
- {
+  int _strcmp(char *s1, char *s2)
+  {
 	 int a = 0, b = 0, c = 0, r = 0, lim;
 
 	 while (s1[a])
@@ -43,5 +43,5 @@
 			 break;
 		 }
 
-		 C++:
+		 C++;
 	}
